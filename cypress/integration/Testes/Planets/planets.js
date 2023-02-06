e26af6a3129d74220405036ea9fb2ee7233c6ef8
Raphael Ribeiro dos Santos <raphael.r2021@gmail.com>
2023-02-06 11:56:47 -0300

    Alteção nos testes de api

---
+++ cypress/integration/Testes/Planets/planets.js
@@ -0,0 +1,19 @@
+import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+import get from "../../../support/serviceExports";
+
+const url = 'https://swapi.dev/api/planets'
+Given('hello', function() {
+  get(url).then(Response =>{
+    cy.wrap(Response).as("results")
+  })
+})
+
+When('i search a planet', function(){
+cy.get("@results").then(res => {
+  const planet = res.body.results
+})
+})
+
+Then('i get a planet Tatooine', function(){
+
+})
